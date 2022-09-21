972ab847abdca4455e3ce48df683ca0b15f00cd5
Adarsh Learner <learner.adarsh@gmail.com>
2022-09-21 17:34:21 +0530

    feat: Added functions to check Regex
    For Email, IPAddress, SSN, Url, Mac address, Phone number, and zipcode

--- index.js
+++ index.js
@@ -1,35 +1,77 @@
 const regexConstants = require('./constants.js');
 
-
-function isEmailValid(value, regexPattern = regexConstants.standardEmailRegex) {
-    return regexPattern.test(value);
+function isValidEmail(value, regexPattern = regexConstants.standardEmailRegex) {
+    try {
+        return regexPattern.test(value);
+    } catch (error) {
+        console.log(`Invalid Regex Format: '${regexPattern}'`, error);
+    }
+    
 }
 
 function isValidIPAddressIPV4(value, regexPattern = regexConstants.IPV4Regex) {
-    return regexPattern.test(value);
+    try {
+        return regexPattern.test(value);
+    } catch (error) {
+        console.log(`Invalid Regex Format: '${regexPattern}'`, error);
+    }
 }
 
 function isValidIPAddressIPV6(value, regexPattern = regexConstants.IPV6Regex) {
-    return regexPattern.test(value);
+    try {
+        return regexPattern.test(value);
+    } catch (error) {
+        console.log(`Invalid Regex Format: '${regexPattern}'`, error);
+    }
 }
 
 function isValidUrl(value, regexPattern = regexConstants.urlRegex) {
-    return regexPattern.test(value);
+    try {
+        return regexPattern.test(value);
+    } catch (error) {
+        console.log(`Invalid Regex Format: '${regexPattern}'`, error);
+    }
 }
 
 function isValidSSN(value, regexPattern = regexConstants.ssnRegex) {
-    return regexPattern.test(value);
+    try {
+        return regexPattern.test(value);
+    } catch (error) {
+        console.log(`Invalid Regex Format: '${regexPattern}'`, error);
+    }
 }
 
 function isValidMACAddress(value, regexPattern = regexConstants.macAddressRegex) {
-    return regexPattern.test(value);
+    try {
+        return regexPattern.test(value);
+    } catch (error) {
+        console.log(`Invalid Regex Format: '${regexPattern}'`, error);
+    }
+}
+
+function isValidPhoneNumber(value, regexPattern = regexConstants.phoneNumberRegx) {
+    try {
+        return regexPattern.test(value);
+    } catch (error) {
+        console.log(`Invalid Regex Format: '${regexPattern}'`, error);
+    }
+}
+
+function isValidZipCode(value, regexPattern = regexConstants.zipCodeRegex) {
+    try {
+        return regexPattern.test(value);
+    } catch (error) {
+        console.log(`Invalid Regex Format: '${regexPattern}'`, error);
+    }
 }
 
 module.exports = {
-    isEmailValid,
+    isValidEmail,
     isValidIPAddressIPV4,
     isValidIPAddressIPV6,
     isValidUrl,
     isValidSSN,
-    isValidMACAddress
+    isValidMACAddress,
+    isValidPhoneNumber,
+    isValidZipCode
 }
