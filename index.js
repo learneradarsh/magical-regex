defacd94803fa01cef5ea0f0d85eebb5672397c5
Adarsh Learner <learner.adarsh@gmail.com>
2022-09-21 16:33:49 +0530

    Init

---
+++ index.js
@@ -0,0 +1,12 @@
+export function isEmailValid(regex = '') {}
+export function isValidIPAddressIPV4() {
+
+}
+
+export function isValidIPAddressIPV6() {
+    
+}
+
+module.exports = {
+    isEmailValid
+}
