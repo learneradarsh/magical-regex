ae3f9ba093b422ca5593ab847276bf7a674f6b19
Adarsh Learner <learner.adarsh@gmail.com>
2022-09-21 16:58:17 +0530

    feat: Added functions to check valid values

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
-export function isEmailValid(regex = '') {}
-export function isValidIPAddressIPV4() {
+const regexConstants = require('./constants.js');
 
+
+function isEmailValid(value, regexPattern = regexConstants.standardEmailRegex) {
+    return regexPattern.test(value);
 }
 
-export function isValidIPAddressIPV6() {
-    
+function isValidIPAddressIPV4(value, regexPattern = regexConstants.IPV4Regex) {
+    return regexPattern.test(value);
+}
+
+function isValidIPAddressIPV6(value, regexPattern = regexConstants.IPV6Regex) {
+    return regexPattern.test(value);
+}
+
+function isValidUrl(value, regexPattern = regexConstants.urlRegex) {
+    return regexPattern.test(value);
+}
+
+function isValidSSN(value, regexPattern = regexConstants.ssnRegex) {
+    return regexPattern.test(value);
+}
+
+function isValidMACAddress(value, regexPattern = regexConstants.macAddressRegex) {
+    return regexPattern.test(value);
 }
 
 module.exports = {
-    isEmailValid
+    isEmailValid,
+    isValidIPAddressIPV4,
+    isValidIPAddressIPV6,
+    isValidUrl,
+    isValidSSN,
+    isValidMACAddress
 }
